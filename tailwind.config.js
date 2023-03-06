71de16325d29152ac5a721e2d7d6675e5df8868a
HelloLee63 <82855877+HelloLee63@users.noreply.github.com>
2023-03-06 12:14:23 +0800

    Install Tailwind.css
    Installed Tailwind.css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
